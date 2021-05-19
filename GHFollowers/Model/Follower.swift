f3ef20b32256c65cb23e634aa45f1c2614026b29
All3yp <noyalle@ualberta.ca>
2021-05-19 20:04:13 -0300

    Create codable models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Alley Pereira on 19/05/21.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
